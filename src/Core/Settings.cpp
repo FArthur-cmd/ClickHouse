a985de16abc1fb760898017a50e0bf2343941023
FArthur-cmd <613623@mail.ru>
2025-05-13 14:35:14 +0300

    improve sampling

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -6738,7 +6738,7 @@ Possible values:
         0 - collect workload for all queries disabled
         1 - collect workload for all queries enabled
     )", EXPERIMENTAL) \
-    DECLARE(Double, sampling_proportion, 0.1, R"(
+    DECLARE(Double, sampling_proportion, 0.01, R"(
 Proportion of data to sample for analysis (0.0 to 1.0). This setting controls how much data is used for sampling operations.
 )", EXPERIMENTAL) \
     DECLARE(UInt64, max_pk_columns_count, 5, R"(
